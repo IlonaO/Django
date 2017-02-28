fe265a821825ab128c0a4deffae4344ec9d87c52
IlonaO <osuch.ilona@gmail.com>
2017-02-28 10:32:38 +0100

    Draft list template and publish method added

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.published_date = timezone.now() #to save posts as a drafts - kill this line!!!
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -39,3 +39,12 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)    
