b84956b84f2b5a98db8d2ea781c4beea087150e1
ilonav8 <osuch.ilona@gmail.com>
2018-09-09 15:32:24 +0200

    Added automaticaly report coverage to HTML

--- manage.py
+++ manage.py
@@ -3,8 +3,24 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	execute_from_command_line(sys.argv)
+
+	is_testing = 'test' in sys.argv
+
+	if is_testing:
+		import coverage
+
+		cov = coverage.coverage()
+		cov.erase()
+		cov.start()
+
+	execute_from_command_line(sys.argv)
+
+	if is_testing:
+		cov.stop()
+		cov.save()
+		cov.html_report()
