deed044c5d1ebe25e6b4a89f6a8780f014b0e62a
ilonav8 <osuch.ilona@gmail.com>
2018-09-09 17:14:40 +0200

    Fix for double execution of tests

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
 	from django.core.management import execute_from_command_line
 
-	execute_from_command_line(sys.argv)
+	# execute_from_command_line(sys.argv)
 
 	is_testing = 'test' in sys.argv
 
