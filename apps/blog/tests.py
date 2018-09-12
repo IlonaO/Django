3a50bb3550e8d8861251b2621876871ebbc6503b
ilonav8 <osuch.ilona@gmail.com>
2018-09-12 11:25:12 +0200

    Removed not used selenium tests from blog

--- apps/blog/tests.py
+++ apps/blog/tests.py
@@ -253,42 +253,8 @@ class ViewsTestCase(TestCase):
 		response = self.client.get('/comment/99/remove/')
 		self.assertEqual(response.status_code, 404)
 
+	def test_vue_post(self):
+		response = self.client.get('/vue_posts/')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'blog/base_vue.html')
 
-# @attr('selenium_test')
-# class BasicTestWithSelenium(TestCase):
-# 	@classmethod
-# 	def setUpClass(cls):
-# 		cls.driver = webdriver.Chrome()
-# 		super(BasicTestWithSelenium, cls).setUpClass()
-#
-# 	@classmethod
-# 	def tearDownClass(cls):
-# 		super(BasicTestWithSelenium, cls).tearDownClass()
-# 		cls.driver.quit()
-#
-# 	def test_if_post_name_is_ok(self):
-# 		"""Verifies the name of fifth post in my local project"""
-#
-# 		self.driver.get('http://127.0.0.1:8000/post/5/')
-# 		name = self.driver.find_element_by_class_name('page-header').text
-# 		self.assertEqual('Z konsoli', name)
-# 		time.sleep(2)
-#
-# 	def test_if_login_link_is_displayed_when_user_is_not_athenticated(self):
-# 		"""Searching if login link is displayed when user isn't logged in"""
-#
-# 		self.driver.get('http://127.0.0.1:8000')
-# 		draft_link = self.driver.find_element_by_partial_link_text('Log in').text
-# 		self.assertEqual('Log in', draft_link)
-# 		time.sleep(2)
-#
-#
-# class PostsAndCommentsTest(LiveServerTestCase):
-# 	def setUp(self):
-# 		self.driver = webdriver.Chrome()
-#
-# 	def tearDown(self):
-# 		self.driver.quit()
-#
-# 	def test_if_authenticated_user_can_add_new_post(self):
-# 		pass
