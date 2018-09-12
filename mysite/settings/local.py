f10ed5241828cfe05ef2da883faabf5f600011d3
ilonav8 <osuch.ilona@gmail.com>
2018-09-12 11:23:51 +0200

    Added pytest

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -1,4 +1,6 @@
 from .base import *
 # from .unit_settings import *
+# from .pytest_settings import *
+
 
 DEBUG = True
--- mysite/settings/unit_settings.py
+++ mysite/settings/unit_settings.py
@@ -15,13 +15,10 @@ TESTS_OUTPUT_PATH = '/tests'
 TEST_RUNNER = 'myvenv.lib.testrunner.NoseTestRunner'
 
 NOSE_ARGS = [
-    '--attr=!selenium_test,!db,!rpc,!todo',
-    # '--with-coverage',
+    '--attr=!selenium_test,!db,!todo',
     '--with-doctest',
+	'--exe',
 ]
-SELENIUM_DIR = os.path.join(BASE_DIR, 'selenium')
-if os.path.exists(SELENIUM_DIR):
-    NOSE_ARGS.append('--exclude-dir=%s' % SELENIUM_DIR)
 
 TEST_OUTPUT_FILE = os.path.join(TESTS_OUTPUT_PATH, 'test-output.html')
 
